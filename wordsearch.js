7e2f51f9c02350e02ae99d111b44a1b087cd9a05
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-03 17:23:35 -0400

    impement vertical word check in wordsearch function by pair programing with @GBiyekenova

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    
+  if (letters.length === 0) return false;
+    
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = new Array(letters[0].length).fill(0).map(() => new Array(letters.length).fill(0));
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      verticalJoin[j][i] = letters[i][j];
     }
-}
+  }
+  const flippedArray = verticalJoin.map((el) => el.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let w of flippedArray) {
+    if (w.includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
 
-module.exports = wordSearch
+
+
+
